53cbbbbc3c0aff251c401e4ac7680c76905298a9
Daksh Rathod <242972250+DHR2206@users.noreply.github.com>
2026-09-21 14:36:04 +0530

    changing the deadline

--- src/data/conference.js
+++ src/data/conference.js
@@ -34,13 +34,8 @@ export const conference = {
     },
     {
       label: "Paper submission deadline",
-    date: (
-  <>
-    <s>20 September 2026</s>
-    <br />
-    20 October 2026
-  </>
-),
+     <s> date: "20 September 2026",</s>
+      date: "20 October 2026",
       note: "Extended from 20 September 2026. Authors should submit full papers before the deadline.",
     },
     {
